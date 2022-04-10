b2071b57696d0a531ce1edf9b768e523e1725ca4
Waikato_Ethan <GGPC@MO>
2022-04-10 21:28:00 +1200

    Add project files.

---
+++ notepad/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using notepad.Models;
+
+namespace notepad.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<notepad.Models.Note> Note { get; set; }
+    }
+}
---
+++ notepad/Models/Note.cs
@@ -0,0 +1,14 @@
+﻿namespace notepad.Models
+{
+    public class Note
+    {
+        //prop tab*2 you will get public int MyProperty {get; set;}
+        public int Id { get; set; }
+        public string NoteTitle { get; set; }
+        public string NoteContent { get; set; }
+        public Note()
+        {
+
+        }
+    }
+}
